b3a5989c4eb2f0f8bb584a210cb60e2374cda975
carl_meyrick <carlmeyrickcheng@gmail.com>
2021-08-18 22:23:26 +0800

    removed duplicate props

--- src/components/calculator/inputs/tipInputBox.js
+++ src/components/calculator/inputs/tipInputBox.js
@@ -3,8 +3,10 @@ import classes from "./tipInputBox.module.css";
 
 const tipInputBox = (props) => {
 
+    const {tip, onChangeTipKeyPress} = props
+
     return (
-        <input type="number" className={classes.input} type="Text" id="tipInput" name="tipInput" placeholder="Custom" value={props.tip} onChange={props.onChangeTipKeyPress}/>
+        <input type="number" className={classes.input} type="Text" id="tipInput" name="tipInput" placeholder="Custom" value={tip} onChange={onChangeTipKeyPress}/>
     );
 
 }
